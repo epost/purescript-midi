274edeacb87ed470fd9f9392794383c6dc10a67a
Erik Post <erik@shinsetsu.nl>
2015-11-25 18:37:53 +0100

    Fix timing in demo function.

--- src/Main.purs
+++ src/Main.purs
@@ -16,15 +16,9 @@ main = do
 play :: forall e. Eff (midi :: Midi.MIDI | e) Unit
 play = do
   Midi.setVolume channel 127
-
-  Midi.noteOn  channel (Midi.toNoteNumber 4 C Natural) velocity (0.0 * delay)
-  Midi.noteOff channel (Midi.toNoteNumber 4 C Natural)          (3.0 * delay)
-
-  Midi.noteOn  channel (Midi.toNoteNumber 4 E Natural) velocity (1.0 * delay)
-  Midi.noteOff channel (Midi.toNoteNumber 4 E Natural)          (3.0 * delay)
-
-  Midi.noteOn  channel (Midi.toNoteNumber 4 G Natural) velocity (2.0 * delay)
-  Midi.noteOff channel (Midi.toNoteNumber 4 G Natural)          (3.0 * delay)
+  Midi.noteOn    channel (Midi.toNoteNumber 4 C Natural) velocity (1.0 * delay)
+  Midi.noteOn    channel (Midi.toNoteNumber 4 E Natural) velocity (2.0 * delay)
+  Midi.noteOn    channel (Midi.toNoteNumber 4 G Natural) velocity (3.0 * delay)
   where
     channel = 0
     note = 127
